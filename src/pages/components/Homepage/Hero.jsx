126a77ee647dfd0e5e2e08a9d123a8ffc25fe761
realstin <justiniratuzimbonyinshuti@gmail.com>
2026-07-18 15:21:33 +0200

    updated the h1 in hero

--- src/pages/components/Homepage/Hero.jsx
+++ src/pages/components/Homepage/Hero.jsx
@@ -2,7 +2,7 @@ function Hero() {
   return (
     <section className="bg-black text-white p-10">
       <h1 className="text-5xl font-bold">
-        Tailwind is working 🎉
+       hero section
       </h1>
     </section>
   );
