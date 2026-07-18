50158f64578c334eeda8475771c234206cf77331
realstin <justiniratuzimbonyinshuti@gmail.com>
2026-07-18 15:50:08 +0200

    changed the hero background colour to white

--- src/pages/components/Homepage/Hero.jsx
+++ src/pages/components/Homepage/Hero.jsx
@@ -1,6 +1,6 @@
 function Hero() {
   return (
-    <section className="bg-black text-white p-10">
+    <section className=" text-white p-10">
       <h1 className="text-5xl font-bold">
        hero section
       </h1>
