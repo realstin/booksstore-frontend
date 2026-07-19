31acb9decaaf6849c9730196fd0573c24392868a
realstin <justiniratuzimbonyinshuti@gmail.com>
2026-07-19 14:25:55 +0200

    fix(css): move global reset into @layer base so Tailwind utilities can override padding/margin

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,6 +1,6 @@
 function Container({ children, className = "" }) {
   return (
-    <div className={`mx-auto w-full max-w-7xl px-8 lg:px-20 ${className}`}>
+    <div className={`mx-auto w-full max-w-7xl px-6 sm:px-8 lg:px-16 ${className}`}>
       {children}
     </div>
   );
