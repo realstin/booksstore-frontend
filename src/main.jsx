eb000b56a4d676881828dfd1cf56ff80753fe9cf
realstin <justiniratuzimbonyinshuti@gmail.com>
2026-09-12 20:28:45 +0200

    fix: load book details action layout styles

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { AuthProvider } from './context/AuthContext'
 import './index.css'
+import './book-details-layout.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
